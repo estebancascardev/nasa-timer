469661a8f0101d46559408b7fa863b650890fbc8
estebancascardev <estebancastrocarias@gmail.com>
2026-09-23 18:23:32 -0400

    feat: add regex to test the inputs, disable inputs and submit button when start, set interval where can count in format 00:00

--- timer.js
+++ timer.js
@@ -1,13 +1,45 @@
-const contador = document.querySelector("p");
+const NUMBER_REGEX = /^(?:[0-9]|[1-5][0-9]|60)$/;
 
-let i = 10;
+const button = document.querySelector("#submit");
+const counter = document.querySelector("p");
+const form = document.querySelector("form");
+const m = document.querySelector("#minute");
+const s = document.querySelector("#second");
 
-const intervalId = setInterval(() =>{
-    if (i!=0){
-        contador.innerHTML = i;
-        i--;
-    } else {
-        clearInterval(intervalId);
-        contador.innerHTML = "Despegue";
-    }
-}, 1000);
+let minute = 0;
+let second = 0;
+
+m.addEventListener("input", e => { 
+    NUMBER_REGEX.test(e.target.value) ? minute = e.target.value : alert("Valor incorrecto. Ingrese un número entre 0 y 60.");
+})
+s.addEventListener("input", e => { 
+    NUMBER_REGEX.test(e.target.value) ? second = e.target.value: alert("Valor incorrecto. Ingrese un número entre 0 y 60.");
+})
+
+form.addEventListener("submit", async e =>{
+    button.setAttribute("disabled", "true")
+    m.setAttribute("disabled", "true");
+    s.setAttribute("disabled", "true");
+    e.preventDefault();
+    counter.innerHTML = `${String(minute).padStart(2, "0")}:${String(second).padStart(2, "0")}`;
+    const intervalId = setInterval(() =>{
+        if (minute>0){
+            if (second==0){
+                second=59;
+                minute--
+                counter.innerHTML = `${String(minute).padStart(2, "0")}:${String(second).padStart(2, "0")}`;
+            } else {
+                second--;
+                counter.innerHTML = `${String(minute).padStart(2, "0")}:${String(second).padStart(2, "0")}`;
+            }
+        } else {
+            if (second!=0){
+                second--;
+                counter.innerHTML = `${String(minute).padStart(2, "0")}:${String(second).padStart(2, "0")}`;
+            } else{
+                counter.innerHTML = `${String(minute).padStart(2, "0")}:${String(second).padStart(2, "0")}`;
+                clearInterval(intervalId);
+            }
+        }
+    }, 1000);
+})
